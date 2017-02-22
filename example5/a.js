97325b8a48bfbf0e35018578aa33674f6d8576b6
LABELNET <1406046087@qq.com>
2017-02-22 13:41:15 +0800

    example5 ： webpack index.js bundle.js

---
+++ example5/a.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yuan on 2/22/2017.
+ */
+function add(x, y) {
+    return x + y;
+}
+
+module.exports.add = add;
---
+++ example5/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by yuan on 2/22/2017.
+ */
+var a=require('./a.js');
+
+console.log(a.add(1,2));
